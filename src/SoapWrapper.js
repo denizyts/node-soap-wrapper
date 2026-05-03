65b11b202730084a49ad5643051daac7049a4562
deniz <denizyetis56@gmail.com>
2026-05-03 23:30:03 +0300

    deepclean the response object (does not return null fields)

--- src/SoapWrapper.js
+++ src/SoapWrapper.js
@@ -4,18 +4,18 @@ const http = require('http');
 
 /*
 author: denocan, denizyts
-*/ 
+*/
 
 //WE WILL GENERATE THE WSDL FILE.
 class SoapWrapper {
-    constructor(serviceName = 'denoService' , portName = 'denoServicePort' , tns = 'http://example.com/soap' , bindingName = 'denoServiceBinding')  {
+    constructor(serviceName = 'denoService', portName = 'denoServicePort', tns = 'http://example.com/soap', bindingName = 'denoServiceBinding') {
         this.serviceName = serviceName;
         this.portName = portName;
         this.tns = tns;
         this.bindingName = bindingName;
     }
 
-    addOperation(operationName, assignFunction, param_types = {} ) {
+    addOperation(operationName, assignFunction, param_types = {}) {
         if (!this.operations) {
             this.operations = [];
         }
@@ -39,40 +39,40 @@ class SoapWrapper {
         const parameterMatch = functionString.match(/\(([^)]*)\)/);
         var parameters = parameterMatch ? parameterMatch[1].split(',').map(param => param.trim()) : [];
 
-        console.log('Parameters:', parameters); 
+        console.log('Parameters:', parameters);
         console.log('Param Types:', param_types);
 
         parameters = parameters.map(param => param.replace(/[{}]/g, ''));   //remove curly braces from parameters.
 
-        console.log('Parameters:', parameters); 
+        console.log('Parameters:', parameters);
 
         const paramsAndTypes = parameters
             .filter(param => param_types[param]) // Filter out parameters without a type
             .map(param => ({
-            name: param,
-            type: param_types[param]
+                name: param,
+                type: param_types[param]
             }));
         // const paramsAndTypes = Object.keys(parameters).map((param) => ({
         //     name: parameters[param],
         //     type: param_types[parameters[param]] || 'string' // Use paramTypes if provided, default to 'string'
         // }));
-    
+
         this.parameters.push({
             operationName: operationName,
             paramsAndTypes: paramsAndTypes
         });
 
 
-       //DENOCAN DEBUG ICIN COK FAYDALI. 
-       // console.log(JSON.stringify(this.parameters, null, 2));
+        //DENOCAN DEBUG ICIN COK FAYDALI. 
+        // console.log(JSON.stringify(this.parameters, null, 2));
 
-        
-    }   
 
-    
-     wsdlGenerator(location){ 
+    }
 
-        this.wsdl =   `<definitions xmlns="http://schemas.xmlsoap.org/wsdl/" 
+
+    wsdlGenerator(location) {
+
+        this.wsdl = `<definitions xmlns="http://schemas.xmlsoap.org/wsdl/" 
              xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/" 
              xmlns:tns="${this.tns}"
              xmlns:xsd="http://www.w3.org/2001/XMLSchema" 
@@ -85,7 +85,7 @@ class SoapWrapper {
         this.wsdl += `<types>
                 <xsd:schema targetNamespace="${this.tns}"
                 xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-                xmlns:ns0="${this.tns}">`; 
+                xmlns:ns0="${this.tns}">`;
 
 
         this.operations.forEach((op) => {
@@ -101,7 +101,7 @@ class SoapWrapper {
             this.wsdl += `</xsd:sequence>
             </xsd:complexType>`;
 
-        });        
+        });
 
         this.operations.forEach((op) => {
             this.wsdl += `<xsd:element name="${op.operationName}Request" type="ns0:${op.operationName}" />
@@ -159,14 +159,14 @@ class SoapWrapper {
         this.wsdl += `</service>`;
         this.wsdl += `</definitions>`;
 
-        this.wsdl = this.wsdl.replace('{' , '');
-        this.wsdl = this.wsdl.replace('}' , '');
+        this.wsdl = this.wsdl.replace('{', '');
+        this.wsdl = this.wsdl.replace('}', '');
         //console.log(this.wsdl);
     }
 
-    createServer(port = 8000 , location = "http://localhost:8000/denizyts") {
+    createServer(port = 8000, location = "http://localhost:8000/denizyts") {
 
-        if (port != location.split(":")[2].split("/")[0]) { 
+        if (port != location.split(":")[2].split("/")[0]) {
             throw new Error("Port and location must be the same. Please check your port and location.");
         }
 
@@ -175,15 +175,15 @@ class SoapWrapper {
         }
 
         //generate wsdl before creating the server !!!
-        this.wsdlGenerator(location); 
-    
+        this.wsdlGenerator(location);
+
         // Construct the service object dynamically
         const service = {
             [this.serviceName]: {
                 [this.portName]: {}
             }
         };
-    
+
         /* For design choice of mkulali, denocan changed his own code and modified it for requirements.
         This part of the wrapper can be design and okey for overriding but be careful and
         inspect other parts of SoapWrapper class. */
@@ -191,16 +191,16 @@ class SoapWrapper {
             service[this.serviceName][this.portName][op.operationName] = (args, callback) => {
                 // Convert the operation function string back to a function
                 //const operationFunction = eval(`(${op.assignFunction})`);
-                
+
                 try {
-                    console.log('Operation:', op.operationName);   
+                    console.log('Operation:', op.operationName);
                     console.log(args);
                     //operationFunction(1,2);    //this is ok.
 
                     //console.log('Operation Function:', operationFunction);
 
-                    const result = op.assignFunction(...Object.values(args) , 1 , 2, 3);
-                    
+                    const result = op.assignFunction(...Object.values(args), 1, 2, 3);
+
 
                     //console.log('Result:', result);
                     //const result = operationFunction(args);
@@ -213,36 +213,36 @@ class SoapWrapper {
 
         this.service = service;   //to get service object from outside of the class. denocan logic.
         //console.log(service);
-        
+
 
         const server = http.createServer((request, response) => {
-            
-            if(request.url === '/author') {
-                response.writeHead(200, {'Content-Type': 'text/plain'});
+
+            if (request.url === '/author') {
+                response.writeHead(200, { 'Content-Type': 'text/plain' });
                 response.end('Author: denocan, denizyts');
                 return;
             }
-            if (request.url === '/?wsdl' || request.url === '/wsdl' ) {
-              response.writeHead(200, {'Content-Type': 'text/xml'});
-              response.end(this.wsdl);
-              return;
-           }
-           
-          else {
-            response.statusCode = 404;  
-            response.end("404: Not Found");
-            if (request.method === 'POST') {
-                let body = '';
-                request.on('data', chunk => {
-                    body += chunk.toString();
-                });
-                request.on('end', () => {
-                    console.log('Request Body:', body);
-                });
+            if (request.url === '/?wsdl' || request.url === '/wsdl') {
+                response.writeHead(200, { 'Content-Type': 'text/xml' });
+                response.end(this.wsdl);
+                return;
             }
-          }
-          });
- 
+
+            else {
+                response.statusCode = 404;
+                response.end("404: Not Found");
+                if (request.method === 'POST') {
+                    let body = '';
+                    request.on('data', chunk => {
+                        body += chunk.toString();
+                    });
+                    request.on('end', () => {
+                        console.log('Request Body:', body);
+                    });
+                }
+            }
+        });
+
         function logRequest(req, res, next) {
             console.log(`Received request for ${req.url}`);
             let body = '';
@@ -254,65 +254,67 @@ class SoapWrapper {
                 next();
             });
         }
-        
+
         server.on('request', (req, res) => {
             logRequest(req, res, () => {
             });
         });
-          
 
-          //error could be occur related to the line below.ERROR COULD BE OCCUR !!!!!! DENOCAN !!!!!! 
-          soap.listen(server, '/'+location.split('/')[3], service, this.wsdl);
-          
-          //console.log('/'+location.split('/')[3]);   //print the listened location. 
 
-          server.listen(port, () => {
+        //error could be occur related to the line below.ERROR COULD BE OCCUR !!!!!! DENOCAN !!!!!! 
+        soap.listen(server, '/' + location.split('/')[3], service, this.wsdl);
+
+        //console.log('/'+location.split('/')[3]);   //print the listened location. 
+
+        server.listen(port, () => {
             console.log(`SOAP service is running at ` + location);
-          });
+        });
 
-          return server;
+        return server;
     }
 
     createServices(location = 'http://localhost:8000/denizyts') {
-         //generate wsdl before creating the server !!!
-         this.wsdlGenerator(location); 
-    
-         // Construct the service object dynamically
-         const service = {
-             [this.serviceName]: {
-                 [this.portName]: {}
-             }
-         };
-     
-         this.operations.forEach(op => {
-             service[this.serviceName][this.portName][op.operationName] = async (args, callback,headers, req) => {
-                 // Convert the operation function string back to a function
-                 //const operationFunction = eval(`(${op.assignFunction})`);
-                 
-                 try {
-                     console.log('Operation:', op.operationName);   
-                     console.log(args);
-                     //operationFunction(1,2);    //this is ok.
- 
-                     //console.log('Operation Function:', operationFunction);
- 
-                     const result = await op.assignFunction(args , req);
-                     
- 
-                     console.log('Result:', result);
-                     //const result = operationFunction(args);
-                     callback(null, result); // Success callback
-                 } catch (err) {
-                     callback(err); // Error callback
-                 }
-             };
-         });
- 
-         this.service = service;   //to get service object from outside of the class. denocan logic.
-         //console.log(service);
-         return service;
+        //generate wsdl before creating the server !!!
+        this.wsdlGenerator(location);
+
+        // Construct the service object dynamically
+        const service = {
+            [this.serviceName]: {
+                [this.portName]: {}
+            }
+        };
+
+        this.operations.forEach(op => {
+            service[this.serviceName][this.portName][op.operationName] = async (args, callback, headers, req) => {
+                // Convert the operation function string back to a function
+                //const operationFunction = eval(`(${op.assignFunction})`);
+
+                try {
+                    const result = await op.assignFunction(args, req);
+                    result = deepclean(result);
+                    callback(null, result); // Success callback
+                } catch (err) {
+                    callback(err); // Error callback
+                }
+            };
+        });
+
+        this.service = service;   //to get service object from outside of the class. denocan logic.
+        //console.log(service);
+        return service;
     }
 
+}
+
+function deepclean(obj) {
+    Object.keys(obj).forEach(key => {
+        if (obj[key] === null || obj[key] === undefined || obj[key] === "") {
+            delete obj[key];
+        } else if (typeof obj[key] === "object") {
+            obj[key] = deepclean(obj[key]);
+        }
+    });
+    return obj;
 }
 
 
